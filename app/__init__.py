61cb910a2ad8a82a7ce1ab2732346892812cdaaf
vanta blanta <vantablanta@gmail.com>
2022-05-18 18:29:15 +0300

    photo upload is working

--- app/__init__.py
+++ app/__init__.py
@@ -2,21 +2,27 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
-
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
+photos = UploadSet('photos',IMAGES)
+
+
 db = SQLAlchemy()
 from .models import Users
+
+
 
 def create_app(config_name):
     app = Flask(__name__)
     
     from config import config_options
     app.config.from_object(config_options[config_name])
-
+    app.config['UPLOADED_PHOTOS_DEST'] = 'static/images/photos'
+    
     from .main import main_blueprint
     app.register_blueprint(main_blueprint)
 
@@ -27,6 +33,8 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     migrate = Migrate(app, db)
+
+    configure_uploads(app,photos)
 
 
     return app
