3d53085bae30eb03b0c432c3b6e0bb82a871dd54
JKLLR <jeffhuria@gmail.com>
2022-05-18 20:32:47 +0300

    add hood page template

--- migrations/versions/7c21001de791_.py
+++ migrations/versions/7c21001de791_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 67fe78589c93
+Revision ID: 7c21001de791
 Revises: 
-Create Date: 2022-05-18 16:09:07.182824
+Create Date: 2022-05-18 20:18:40.890370
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '67fe78589c93'
+revision = '7c21001de791'
 down_revision = None
 branch_labels = None
 depends_on = None
